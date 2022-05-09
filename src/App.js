fef1b60293f136fafa621d747b97c3f6f8cc777f
dyyllaann <dylan.arveson@gmail.com>
2022-05-09 13:07:27 -0700

    Improve routing.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@
 import React, { Component } from 'react';
 import Menu from './components/navbar/Header-Menu';
 import Tasks from "./components/tasks/Tasks";
-import People from './components/people/People';
 
 class App extends Component {
   constructor() {
@@ -26,9 +25,9 @@ class App extends Component {
     return (
 			<div className="App">
 				<Menu handleChange={this.handleChange} />
-        {this.state.activePage === 0 && <People />}
+        {this.state.activePage === 0 && <p>Calendar</p>}
         {this.state.activePage === 1 && <Tasks />}
-        {this.state.activePage === 2 && <p>Page 3</p>}
+        {this.state.activePage === 2 && <p>Time Reports</p>}
 			</div>
 		);
   }
--- src/components/people/Contact.js
+++
@@ -1,7 +0,0 @@
-export default function Contact(firstName, lastName) {
-	this.firstName = firstName;
-  this.lastName = lastName;
-  this.dateAdded = new Date();
-	this.details = [];
-	this.id = crypto.randomUUID();
-}
